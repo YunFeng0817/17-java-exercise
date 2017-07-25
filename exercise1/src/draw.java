f28f660e0a5a0c25136cf9f0658b24074b6eef55
Fitz <f18846188605@gmail.com>
2017-07-26 02:00:21 +0800

    I change the colors of five circles in draw.java

--- exercise1/src/draw.java
+++ exercise1/src/draw.java
@@ -13,15 +13,15 @@ public class draw extends JFrame{
     private  JPanel pane = new JPanel();
 
     public void paint(Graphics g){
-        g.setColor(new Color(0,0,0));
-        g.drawOval(start, start, rand, rand);//绘制第1个圆形
-        g.setColor(new Color(255,0,0));
-        g.drawOval(start+rand-10, start, rand, rand);//绘制第2个圆形
-        g.setColor(new Color(255,255,0));
-        g.drawOval(start+2*(rand-10), start, rand, rand);//绘制第3个圆形
-        g.setColor(new Color(0,255,0));
-        g.drawOval(start+rand-50, start+60, rand, rand);//绘制第4个圆形
         g.setColor(new Color(0,0,255));
+        g.drawOval(start, start, rand, rand);//绘制第1个圆形
+        g.setColor(new Color(0,0,0));
+        g.drawOval(start+rand-10, start, rand, rand);//绘制第2个圆形
+        g.setColor(new Color(255,0,0));
+        g.drawOval(start+2*(rand-10), start, rand, rand);//绘制第3个圆形
+        g.setColor(new Color(255,255,0));
+        g.drawOval(start+rand-50, start+60, rand, rand);//绘制第4个圆形
+        g.setColor(new Color(0,255,0));
         g.drawOval(start+2*rand-50, start+60, rand, rand);//绘制第5个圆形
     }
 
