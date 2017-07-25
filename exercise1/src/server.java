444697507c26bac7061f2fa3bcb2acaa06dc3eb6
Fitz <f18846188605@gmail.com>
2017-07-25 22:54:56 +0800

    I complete the exercise almost but still has some bugs

--- exercise1/src/server.java
+++ exercise1/src/server.java
@@ -7,23 +7,49 @@ public class server {
     public static void main(String[] args) {
         try {
             ServerSocket myserver = new ServerSocket(8000);
+            ServerSocket myAnotherServer = new ServerSocket(7000);
             Socket listening = myserver.accept();
+            Socket listen = myAnotherServer.accept();
 
             BufferedReader input = new BufferedReader(new InputStreamReader(listening.getInputStream()));
+            BufferedReader in = new BufferedReader(new InputStreamReader(listen.getInputStream()));
 
             PrintWriter output = new PrintWriter(listening.getOutputStream(), true);
+            PrintWriter out = new PrintWriter(listen.getOutputStream(),true);
 
+            Thread onetoanother = new Thread(){
+              public void run(){
+                  try{
+                      String getWords = "";
+                      while(true) {
+                          getWords = input.readLine();
+                          if (getWords.equals("BYE"))
+                          {
+                              System.exit(0);
+                          }
+                          if(!getWords.equals("")){
+                              out.println(getWords);
+                          }
+                      }
+                  }
+                  catch (IOException e){
+                      System.out.println(e);
+                  }
+
+              }
+            };
+
+            onetoanother.start();
             while (true) {
-                String getWords = "";
                 String pushWords = "";
-                getWords = input.readLine();
-                System.out.println(getWords);
-                System.out.println("请输入你想说的话");
-                Scanner in = new Scanner(System.in);
-                pushWords = in.nextLine();
-                if(pushWords.equals("BYE"))
+                pushWords = in.readLine();
+                if (pushWords.equals("BYE"))
+                {
                     System.exit(0);
-                output.println(pushWords);
+                }
+                if(!pushWords.equals("")){
+                    output.println(pushWords);
+                }
             }
         } catch (IOException e) {
             System.err.println(e);
